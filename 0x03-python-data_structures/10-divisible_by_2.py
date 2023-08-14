9b3bff6b5aa223d84285f3b0cfbf1affbe0a870e
joseph9916 <ebunopeajayi@gmail.com>
2023-08-14 10:24:51 +0000

    Only by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     new_list = list(my_list)
-    for num in new_list:
-        num = num % 2 
+    for i in range(len(my_list)):
+        new_list[i] = my_list[i] % 2 == 0
     return new_list
