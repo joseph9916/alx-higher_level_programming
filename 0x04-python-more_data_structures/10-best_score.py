69cdde401247f24a36a8774d8248c45194b325b5
joseph9916 <ebunopeajayi@gmail.com>
2023-08-18 15:06:27 +0000

    Best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    keys = list(a_dictionary.keys())
+    largest_key = keys[0]
+    for key in keys:
+        if a_dictionary[key] > a_dictionary[largest_key]:
+            largest_key = key
+    return largest_key
