29739062507a83f0f7bd528b21574ae62a5481c4
joseph9916 <ebunopeajayi@gmail.com>
2023-08-21 10:55:32 +0000

    Tuples addition

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -15,6 +15,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
     else:
         if len(tuple_a) == 1 and len(tuple_b) > 1:
             return (tuple_a[0] + tuple_b[0], tuple_b[1])
+        elif len(tuple_a) == 1 and len(tuple_b) == 1:
+            return (tuple_a[0] + tuple_b[0], 0)
         elif len(tuple_a) > 1 and len(tuple_b) == 1:
             return (tuple_a[0] + tuple_b[0], tuple_a[1])
         else:
