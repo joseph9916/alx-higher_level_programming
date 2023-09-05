254a76872ffb86d13db14102c947fd9d05b80484
joseph9916 <ebunopeajayi@gmail.com>
2023-09-05 21:32:46 +0000

    tests driven python programming

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,34 @@
+import sys
+"""
+Write a function that prints a text with 2 new lines
+after each of these characters: ., ? and :
+"""
+
+
+def text_indentation(text):
+    """
+    text must be a string, otherwise raise a TypeError
+    There should be no space at the beginning or at the
+    end of each printed line
+    """
+    
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    string = ""
+    i = 0
+    while i < len(text):
+        if text[i] in ".?:":
+            string += "{}{}".format(text[i], "\n" * 2)
+            i += i < len(text) and 1
+            while i < len(text) and text[i] in " \t":
+                i += 1
+        else:
+            string += "{}".format(text[i])
+            i += 1
+
+    sys.stdout.write(string)
+    
+
+if __name__ == "__main__":
+    import doctest
+    doctest("tests/5-text_indentation.txt")
