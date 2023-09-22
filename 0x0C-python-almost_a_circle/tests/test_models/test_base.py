299ca4021226c3b4b09592ade583a797f9155a15
joseph9916 <ebunopeajayi@gmail.com>
2023-09-22 18:41:54 +0000

    unittests added

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -6,14 +6,11 @@ tests for the base class
 
 import unittest
 from models.base import Base
-from models.rectangle import Rectangle
-from models.square import Square
 
 
 b1 = Base()
-b2 = Base(15)
-b3 = Base("2")
-r1 = Rectangle(10, 2)
+b2 = Base()
+b3 = Base(15)
 
 class TestBase(unittest.TestCase):
     """Inherited class to test max integer"""
@@ -21,11 +18,10 @@ class TestBase(unittest.TestCase):
         """Tests if the base class is created and id is assigned accurately"""
         self.assertIsInstance(b1, Base)
         self.assertIsInstance(b2, Base)
-        self.assertIsNotInstance(r1, Base)
 
     def test_values(self):
         """Test if the base class values have been updated"""
         self.assertEqual(b1.id, 1)
-        self.assertEqual(b2.id, 15)
-        self.assertEqual(b3.id, "2")
+        self.assertEqual(b2.id, 2)
+        self.assertEqual(b3.id, 15)
         self.assertNotEqual(b1.id, None)
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -10,31 +10,61 @@ from models.rectangle import Rectangle
 from models.square import Square
 
 
-b1 = Base()
-b2 = Base(15)
-b3 = Base("2")
 r1 = Rectangle(10, 2)
+r2 = Rectangle(5, 3)
 r3 = Rectangle(10, 2, 1, 1, 12)
 
 class TestBase(unittest.TestCase):
     """Inherited class to test max integer"""
-    def test_base(self):
+    def test_rect(self):
         """Tests if the base class is created and id is assigned accurately"""
-        self.assertIsInstance(b1, Base)
-        self.assertIsInstance(b2, Base)
-        self.assertEqual(b1.id, 1)
-        self.assertEqual(b2.id, 15)
-        self.assertEqual(b3.id, "2")
+        self.assertIsInstance(r1, Base)
+        self.assertIsInstance(r2, Rectangle)
 
     def test_init_rectangle(self):
         """Test  the initialising of rectangle"""
         self.assertIsInstance(r1, Rectangle)
         self.assertIsInstance(r3, Rectangle)
-        self.assertEqual(r1.id, 2)
+        self.assertEqual(r1.id, 3)
         self.assertEqual(r3.id, 12)
         self.assertEqual(r1.width, 10)
         self.assertEqual(r1.height, 2)
         self.assertEqual(r1.x, 0)
         self.assertEqual(r1.y, 0)
-        self.assertEqual(r2.x, 1)
-        self.assertEqual(r2.y, 1)
+        self.assertEqual(r2.x, 0)
+        self.assertEqual(r2.y, 0)
+
+    def test_raise_error(self):
+        """Test if it raises errors"""
+        self.assertRaises(TypeError, Rectangle, ("1", "2"))
+
+    #def test_value_error(self):
+        """Test if it raises errors"""
+     #   self.assertRaises(ValueError, Rectangle, (1, -2))
+
+    def test_area(self):
+        self.assertAlmostEqual(r1.area(), 20)
+        self.assertAlmostEqual(r2.area(), 15)
+
+    def test_update(self):
+        """test updates"""
+        vals = (6, 6, 20, 7, 8)
+        r1.update(*vals)
+        self.assertAlmostEqual(r1.id, 6)
+        self.assertAlmostEqual(r1.width, 6)
+        self.assertAlmostEqual(r1.height, 20)
+        self.assertAlmostEqual(r1.x, 7)
+        self.assertAlmostEqual(r1.y, 8)
+        kwargs = {"id": 1, "width": 3, "height": 4, "x": 3, "y": 5}
+        r1.update(**kwargs)
+        self.assertAlmostEqual(r1.id, 1)
+        self.assertAlmostEqual(r1.width, 3)
+        self.assertAlmostEqual(r1.height, 4)
+        self.assertAlmostEqual(r1.x, 3)
+        self.assertAlmostEqual(r1.y, 5)
+        kwargs = {"width": -2}
+        self.assertRaises(ValueError, r1.update, **kwargs)
+        kwargs = {"width": "i"}
+        self.assertRaises(TypeError, r1.update, **kwargs)
+        args = (3, -2, 3)
+        self.assertRaises(ValueError, r1.update, *args)
