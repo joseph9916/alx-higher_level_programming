a1adbd3a1a3239293fce4ebad13098bc8fd6dbb6
joseph9916 <ebunopeajyi@gmail.com>
2023-10-17 18:12:31 +0100

    tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/bin/usr/python3
 """
 tests for the base class
 """
@@ -10,10 +10,13 @@ from models.base import Base
 
 b1 = Base()
 b2 = Base()
-b3 = Base(15)
+b3 = Base(97)
+b4 = Base()
+dictionary = {'x': 2, 'width': 10, 'id': 1, 'height': 7, 'y': 8}
+json_string = '[{"x": 2, "width": 10, "id": 1, "height": 7, "y": 8}]'
 
 class TestBase(unittest.TestCase):
-    """Inherited class to test max integer"""
+    """Inherited class to test base"""
     def test_base(self):
         """Tests if the base class is created and id is assigned accurately"""
         self.assertIsInstance(b1, Base)
@@ -23,5 +26,17 @@ class TestBase(unittest.TestCase):
         """Test if the base class values have been updated"""
         self.assertEqual(b1.id, 1)
         self.assertEqual(b2.id, 2)
-        self.assertEqual(b3.id, 15)
+        self.assertEqual(b3.id, 97)
+        self.assertEqual(b4.id, 3)
         self.assertNotEqual(b1.id, None)
+
+    def test_to_json_string(self):
+        """Test to_json_string"""
+        self.assertEqual(Base.to_json_string([dictionary]), json_string)
+        self.assertEqual(Base.to_json_string(None), "[]")
+        self.assertEqual(Base.to_json_string([]), "[]")
+
+    def test_from_json_string(self):
+        """Test from_json_string"""
+        self.assertEqual(Base.from_json_string(json_string), [dictionary])
+        self.assertEqual(Base.from_json_string("[]"), [])
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,70 +1,126 @@
-#!/usr/bin/python3
+#!/bin/usr/python3
 """
-tests for the base class
+tests for the Rectangle class
 """
 
 
 import unittest
-from models.base import Base
 from models.rectangle import Rectangle
-from models.square import Square
+from models.base import Base
 
 
-r1 = Rectangle(10, 2)
-r2 = Rectangle(5, 3)
-r3 = Rectangle(10, 2, 1, 1, 12)
+r1 = Rectangle(1, 2)
+r2 = Rectangle(1, 2, 3, 4, 5)
+b1 = Base()
+return_string = "[Rectangle] (1) 0/0 - 1/2"
+display1 = "{}".format("#\n" * 2)
+display2 = "{}{}".format("\n" * 4, "#\n" * 2)
+r1_dictionary = {"id": 1, "width": 1, "height": 2, "x": 0, "y": 0}
 
-class TestBase(unittest.TestCase):
-    """Inherited class to test max integer"""
-    def test_rect(self):
-        """Tests if the base class is created and id is assigned accurately"""
-        self.assertIsInstance(r1, Base)
-        self.assertIsInstance(r2, Rectangle)
-
-    def test_init_rectangle(self):
-        """Test  the initialising of rectangle"""
+class TestRectangle(unittest.TestCase):
+    """Inherited class to test rectangle objects"""
+    def test_rectangle(self):
+        """Tests if the rectangle class is created"""
         self.assertIsInstance(r1, Rectangle)
-        self.assertIsInstance(r3, Rectangle)
-        self.assertEqual(r1.id, 3)
-        self.assertEqual(r3.id, 12)
-        self.assertEqual(r1.width, 10)
+        self.assertIsInstance(r2, Rectangle)
+        self.assertIsInstance(b1, Base)
+
+    def test_values(self):
+        """Tests if Rectangle class values have been correctly assigned"""
+        self.assertEqual(r1.id, 1)
+        self.assertEqual(r2.id, 5)
+        self.assertEqual(r1.width, 1)
         self.assertEqual(r1.height, 2)
         self.assertEqual(r1.x, 0)
         self.assertEqual(r1.y, 0)
-        self.assertEqual(r2.x, 0)
-        self.assertEqual(r2.y, 0)
+        self.assertEqual(r2.width, 1)
+        self.assertEqual(r2.height, 2)
+        self.assertEqual(r2.x, 3)
+        self.assertEqual(r2.y, 4)
+        self.assertNotEqual(r1.width, 0)
+        self.assertNotEqual(r1.height, 0)
 
-    def test_raise_error(self):
-        """Test if it raises errors"""
-        self.assertRaises(TypeError, Rectangle, ("1", "2"))
-
-    #def test_value_error(self):
-        """Test if it raises errors"""
-     #   self.assertRaises(ValueError, Rectangle, (1, -2))
+    def test_error_attributes(self):
+        """Test if Rectangle class raises error when assigning wrong
+        attributes"""
+        self.assertRaises(ValueError, Rectangle, 0, 0, 1, 2)
+        self.assertRaises(ValueError, Rectangle, 1, 2, -1, 2)
+        self.assertRaises(ValueError, Rectangle, -1, 2, 1, 2)
+        self.assertRaises(ValueError, Rectangle, 1, 2, 1, -2)
+        self.assertRaises(TypeError, Rectangle, "0", 0, 1, 2)
+        self.assertRaises(TypeError, Rectangle, 1, "0", 1, 2)
+        self.assertRaises(TypeError, Rectangle, 3, 2, "1", 2)
+        self.assertRaises(TypeError, Rectangle)
 
     def test_area(self):
-        self.assertAlmostEqual(r1.area(), 20)
-        self.assertAlmostEqual(r2.area(), 15)
+        """Test area of rectangle"""
+        self.assertEqual(r1.area(), 2)
+
+    def test_str(self):
+        """Test string of rectangle"""
+        self.assertEqual(r1.__str__(), return_string)
+
+    def test_display(self):
+        """Test display of rectangle"""
+        self.assertEqual(r1.display(), print(display1))
+        self.assertEqual(r2.display(), print(display2))
+
+    def test_to_dictionary(self):
+        """Tests to dictionary of Rectangle"""
+        self.assertDictEqual(r1.to_dictionary(), r1_dictionary)
 
     def test_update(self):
-        """test updates"""
-        vals = (6, 6, 20, 7, 8)
-        r1.update(*vals)
-        self.assertAlmostEqual(r1.id, 6)
-        self.assertAlmostEqual(r1.width, 6)
-        self.assertAlmostEqual(r1.height, 20)
-        self.assertAlmostEqual(r1.x, 7)
-        self.assertAlmostEqual(r1.y, 8)
-        kwargs = {"id": 1, "width": 3, "height": 4, "x": 3, "y": 5}
-        r1.update(**kwargs)
-        self.assertAlmostEqual(r1.id, 1)
-        self.assertAlmostEqual(r1.width, 3)
-        self.assertAlmostEqual(r1.height, 4)
-        self.assertAlmostEqual(r1.x, 3)
-        self.assertAlmostEqual(r1.y, 5)
-        kwargs = {"width": -2}
-        self.assertRaises(ValueError, r1.update, **kwargs)
-        kwargs = {"width": "i"}
-        self.assertRaises(TypeError, r1.update, **kwargs)
-        args = (3, -2, 3)
-        self.assertRaises(ValueError, r1.update, *args)
+        """Tests updates of dictionary"""
+        r3 = Rectangle(1, 2)
+        r3.update()
+        r3_string = "[Rectangle] (14) 0/0 - 1/2"
+        self.assertEqual(r3.__str__(), r3_string)
+        r3.update(89)
+        r3_string = "[Rectangle] (89) 0/0 - 1/2"
+        self.assertEqual(r3.__str__(), r3_string)
+        r3.update(89, 4)
+        r3_string = "[Rectangle] (89) 0/0 - 4/2"
+        self.assertEqual(r3.__str__(), r3_string)
+        r3.update(89, 4, 5)
+        r3_string = "[Rectangle] (89) 0/0 - 4/5"
+        self.assertEqual(r3.__str__(), r3_string)
+        r3.update(89, 4, 5, 1)
+        r3_string = "[Rectangle] (89) 1/0 - 4/5"
+        self.assertEqual(r3.__str__(), r3_string)
+        r3.update(89, 4, 5, 1, 2)
+        r3_string = "[Rectangle] (89) 1/2 - 4/5"
+        self.assertEqual(r3.__str__(), r3_string)
+        r4 = Rectangle(1, 2)
+        r4.update(**{"id": 100})
+        r4_string = "[Rectangle] (100) 0/0 - 1/2"
+        self.assertEqual(r4.__str__(), r4_string)
+        r4.update(**{"id": 100, "width": 4})
+        r4_string = "[Rectangle] (100) 0/0 - 4/2"
+        self.assertEqual(r4.__str__(), r4_string)
+        r4.update(**{"id": 100, "width": 4, "height": 5})
+        r4_string = "[Rectangle] (100) 0/0 - 4/5"
+        self.assertEqual(r4.__str__(), r4_string)
+        r4.update(**{"id": 100, "width": 4, "height": 5, "x": 2})
+        r4_string = "[Rectangle] (100) 2/0 - 4/5"
+        self.assertEqual(r4.__str__(), r4_string)
+        r4.update(**{"id": 100, "width": 4, "height": 5, "x": 2, "y": 3})
+        r4_string = "[Rectangle] (100) 2/3 - 4/5"
+        self.assertEqual(r4.__str__(), r4_string)
+
+    def test_create(self):
+        """Tests if Rectangle creates method creates a rectangle"""
+        r5 = Rectangle.create(**{'id': 89, 'width': 4})
+        r5_string = "[Rectangle] (89) 0/0 - 4/2"
+        self.assertEqual(r5.__str__(), r5_string)
+        r6 = Rectangle.create(**{'id': 89})
+        r6_string = "[Rectangle] (89) 0/0 - 1/2"
+        self.assertEqual(r5.__str__(), r5_string)
+        r7 = Rectangle.create(**{'id': 89, 'width': 4, "height": 3})
+        r7_string = "[Rectangle] (89) 0/0 - 4/3"
+        self.assertEqual(r7.__str__(), r7_string)
+        r8 = Rectangle.create(**{'id': 89, 'width': 4, "height": 3, "x": 1})
+        r8_string = "[Rectangle] (89) 1/0 - 4/3"
+        self.assertEqual(r8.__str__(), r8_string)
+        r9 = Rectangle.create(**{'id': 89, 'width': 4, "height": 3, "x": 1, "y": 2})
+        r9_string = "[Rectangle] (89) 1/2 - 4/3"
+        self.assertEqual(r9.__str__(), r9_string)
