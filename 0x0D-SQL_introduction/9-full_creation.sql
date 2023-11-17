f6741a7be10ee7a0b2d70d34e5c34984d7ae923c
joseph9916 <ebunopeajyi@gmail.com>
2023-11-17 05:06:12 +0100

    sql programming

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,19 @@
+/* 
+	creates a table called first_table in the current database in your MySQL
+	first_table description:
+		id INT
+		name VARCHAR(256)
+ */
+DROP TABLE IF EXISTS second_table;
+CREATE TABLE second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+INSERT INTO second_table
+(id, name, score)
+VALUES 
+	(1, 'John', 10),
+	(2, 'Alex', 3),
+	(3, 'Bob', 14),
+	(4, 'George', 8);
