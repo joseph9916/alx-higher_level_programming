a2ab6aac643cdcc669e1b0f3ff213ed316291f58
joseph9916 <ebunopeajyi@gmail.com>
2024-01-06 22:40:38 +0100

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #Write a Bash script that takes in a URL, sends a request to that URL,
-#and displays the size of the body of the response
-
-curl -w '%{size_request}' -s -o /dev/null "$1"
-echo ""
+curl -w '%{size_request}' -s -o /dev/null "$1"; echo ""
