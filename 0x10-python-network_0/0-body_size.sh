4428d59597eaecf6c54e2ad9700d96eae3ac8b17
joseph9916 <ebunopeajyi@gmail.com>
2024-01-07 00:20:46 -0800

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Write a Bash script that takes in a URL, sends a request to that URL,
-curl -w '%{size_request}' -s -o /dev/null "$1"; echo ""
+curl -w '%{size_download}' -s -o /dev/null "$1"; echo ""
