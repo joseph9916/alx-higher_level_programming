5b5ca6e4c58b5358c00792c4a0089cba159603ab
joseph9916 <ebunopeajayi@gmail.com>
2024-05-29 09:54:43 +0100

    deferring my learning

--- 0x12-javascript-warm_up/10-factorial.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const { argv } = require('node:process');
-const num = Number(argv[2]);
-
-function fact (num) {
-  if (num === 1 || num === 0 || isNaN(num)) { return 1; }
-  return num * fact(num - 1);
-}
-console.log(fact(num));
--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const { argv } = require('node:process');
-const num = Number(argv[2]);
-
-if (isNaN(num)) {
-  console.log('Not a number');
-} else {
-  console.log('My number: ' + num);
-}
